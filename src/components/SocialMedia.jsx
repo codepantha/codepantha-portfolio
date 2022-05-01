6df597cd340ec2ed83733c50f717971672e9821b
codepanther <pezechigozirim@gmail.com>
2022-05-01 02:21:04 +0100

    create SocialMedia.jsx component

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs';
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+      <div>
+        <BsTwitter />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+      <div>
+        <BsLinkedin />
+      </div>
+    </div>
+  )
+}
+
+export default SocialMedia;
