f4993dbe4d36261e5bd7a66b1144a4d007a23097
codepanther <pezechigozirim@gmail.com>
2022-05-01 02:20:46 +0100

    create NavigationDots component

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className="app__navigation">
+      {['home', 'about', 'work', 'skills', 'contact'].map((item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className="app__navigation-dot"
+          style={active === item ? { backgroundColor: '#1a3c40' } : {}}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default NavigationDots;
