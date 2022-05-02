68896aa99cd41367d88fa9e3ec222d2a554f3112
codepanther <pezechigozirim@gmail.com>
2022-05-02 13:54:34 +0100

    fix projects not showing on intial render

--- src/containers/Work/Work.jsx
+++ src/containers/Work/Work.jsx
@@ -15,25 +15,23 @@ const Work = () => {
     const query = '*[_type == "works"]';
 
     client.fetch(query).then((data) => {
-      setWorks(data)
+      setWorks(data);
       setFilteredWorks(data);
     });
   }, []);
 
-  useEffect(() => {
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
     setAnimateCard({ y: 100, opacity: 0 });
 
     setTimeout(() => {
       setAnimateCard({ y: 0, opacity: 1 });
 
-      if (activeFilter === 'All') return setFilteredWorks(works)
-      
-      setFilteredWorks(works.filter(work => work.tags.includes(activeFilter)))
+      if (item === 'All') return setFilteredWorks(works);
+
+      setFilteredWorks(works.filter((work) => work.tags.includes(item)));
     }, 500);
-
-  }, [activeFilter]);
-
-  const handleWorkFilter = (item) => setActiveFilter(item);
+  };
 
   return (
     <section>
@@ -42,17 +40,19 @@ const Work = () => {
       </h2>
 
       <ul className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React', 'All'].map((item, index) => (
-          <li
-            className={`app__work-filter-item app__flex ${
-              activeFilter === item ? 'item-active' : ''
-            }`}
-            key={item + index}
-            onClick={() => handleWorkFilter(item)}
-          >
-            {item}
-          </li>
-        ))}
+        {['UI/UX', 'Web App', 'Mobile App', 'React', 'All'].map(
+          (item, index) => (
+            <li
+              className={`app__work-filter-item app__flex ${
+                activeFilter === item ? 'item-active' : ''
+              }`}
+              key={item + index}
+              onClick={() => handleWorkFilter(item)}
+            >
+              {item}
+            </li>
+          )
+        )}
       </ul>
 
       <motion.section
@@ -64,16 +64,20 @@ const Work = () => {
           <article className="app__work-item app__flex" key={work + index}>
             <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.title} />
-              
+
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.5, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{
+                  duration: 0.5,
+                  ease: 'easeInOut',
+                  staggerChildren: 0.5
+                }}
                 className="app__work-hover app__flex"
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     animate={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -82,8 +86,8 @@ const Work = () => {
                 </a>
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
-                    animate={{ scale: [0,1] }}
-                    whileHover={{ scale: [1, 0.90 ]}}
+                    animate={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -95,17 +99,19 @@ const Work = () => {
 
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
-              
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
+
               <motion.div
                 animate={{ opacity: [0, 1] }}
                 transition={{ duration: 0.3 }}
                 className="app__work-tag app__flex"
               >
-                {work.tags.map(tag => (
+                {work.tags.map((tag) => (
                   <motion.div
                     animate={{ scale: [0.5, 1] }}
-                    whileHover={{ scale: [1, 1.05 ]}}
+                    whileHover={{ scale: [1, 1.05] }}
                     transition={{ duration: 0.25 }}
                   >
                     <p className="p-text">{tag}</p>
