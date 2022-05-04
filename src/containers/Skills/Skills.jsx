73b658ead24b3d68c042be737cbb5e1bf982ff2b
codepanther <pezechigozirim@gmail.com>
2022-05-04 15:00:50 +0100

    create Skills component html structure and fetch dynamic data

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+// import ReactTooltip from 'react-tooltip';
+// import { AppWrap } from '../../wrapper';
+import { client, urlFor } from '../../client';
 import './Skills.scss';
+import { AppWrap } from '../../wrapper';
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+  const [skills, setSkills] = useState([]);
 
-export default Skills
+  useEffect(() => {
+    const query = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => setSkills(data));
+  }, []);
+
+  return (
+    <section>
+      <h2 className="head-text">
+        My <span>Skills</span>
+      </h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.ul
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.ul>
+          ))}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default AppWrap(Skills, 'skills');
