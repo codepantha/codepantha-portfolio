39bd1a84e6682b63699e93a825763baee810e015
codepanther <pezechigozirim@gmail.com>
2022-05-06 18:04:17 +0100

    change animation style on Skills component and on MotionWrap HOC

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -25,7 +25,7 @@ const Skills = () => {
         <motion.div className="app__skills-list">
           {skills.map((skill) => (
             <motion.ul
-              animate={{ opacity: [0, 1] }}
+              whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
               key={skill.name}
